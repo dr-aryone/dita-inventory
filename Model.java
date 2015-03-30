ce2b94487b47c3c5e264e5a97c8735923d7866f2
dita-programming <dita.programming@gmail.com>
2015-03-31 01:59:08 +0300

    Update Model.java

--- Model.java
+++ Model.java
@@ -5,13 +5,51 @@
  import java.sql.*;
  import java.lang.reflect.Method;
  
- public class Method {
+ public class Model {
+    private static String DRIVER = "com.mysql.jdbc.Driver";
+    private static String URL = "jdbc:mysql://localhost/sports";
+    private static String USER = "root";
+    private static String PASSWORD = "root";
     private Connection conn = null;
     private Statement statement = null;
     private PreparedStatement preparedStatement = null;
     private ResultSet result = null;
     
-    public transactions(Method trnsctn, Object...args) {
-        //
+    public void transactions(Model obj,Method t, Object...args) {
+        t.invoke(obj,args);
+    }
+    
+    private void startConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(URL,USER,PASSWORD);
+            System.out.println("Connection established");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+    
+    private void closeConnection(){
+        try {
+            if(result != null) {
+                result.close();
+            }
+            
+            if(statement != null) {
+                statement.close();
+            }
+            
+            if(preparedStatement != null) {
+                preparedStatement.close();
+            }
+            
+            if(conn != null) {
+                conn.close()
+            }
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
 }
