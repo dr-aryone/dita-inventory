1f04c3cf8587f21f5ed6344fec9a54343c43e114
dita-programming <dita.programming@gmail.com>
2015-03-31 01:37:59 +0300

    Create Model.java

---
+++ Model.java
@@ -0,0 +1,17 @@
+/*
+ * This is the main model of the program. All methods for accessing 
+ * and manipulating the database are defined here
+ */
+ import java.sql.*;
+ import java.lang.reflect.Method;
+ 
+ public class Method {
+    private Connection conn = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet result = null;
+    
+    public transactions(Method trnsctn, Object...args) {
+        //
+    }
+}
