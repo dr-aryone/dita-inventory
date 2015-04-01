6b773f80cbf62b6beaa04b01787a05ee5a0fe336
michael <michaelbukachi@gmail.com>
2015-04-01 13:47:54 +0300

    Adde createTables method to model

--- Inventory/src/Model.java
+++ Inventory/src/Model.java
@@ -34,7 +34,49 @@ public class Model {
 		/*
 		 * Creates tables in the database if they don't exist
 		 */
-		//
+		String sql;
+		try {
+			statement = conn.createStatement();
+			sql = "CREATE TABLE IF NOT EXISTS Teams" +
+				  "(name VARCHAR(30) NOT NULL," +
+				  "total_members INTEGER NOT NULL," +
+				  "PRIMARY KEY(name))";
+			statement.addBatch(sql);
+			
+			
+			sql = "CREATE TABLE IF NOT EXISTS Members" +
+				  "(name VARCHAR(30) NOT NULL," +
+				  "team VARCHAR(30) NOT NULL," +
+				  "password VARCHAR(50) NOT NULL," +
+				  "PRIMARY KEY(name)," +
+				  "FOREIGN KEY(team) REFERENCES Teams(name))";
+			statement.addBatch(sql);
+			
+			sql = "CREATE TABLE IF NOT EXISTS Items" +
+				  "(name VARCHAR(20) NOT NULL," +
+				  "quantity INTEGER NOT NULL," +
+				  "current_quantity INTEGER NOT NULL," +
+				  "PRIMARY KEY(name))";
+			statement.addBatch(sql);
+			
+			sql = "CREATE TABLE IF NOT EXISTS Log" +
+				  "(indx INTEGER NOT NULL," +
+				  "item VARCHAR(20) NOT NULL," +
+				  "time_out TIME NOT NULL," +
+				  "time_in TIME DEFAULT NULL," +
+				  "name VARCHAR(30) NOT NULL," +
+				  "quantity INTEGER NOT NULL," +
+				  "PRIMARY KEY(indx)," +
+				  "FOREIGN KEY(name) REFERENCES Members(name))";
+			statement.addBatch(sql);
+		
+			statement.executeBatch();
+			System.out.println("Tables created successfully");
+		} catch(Exception e) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		    //System.exit(0);
+		}
+		
 	}
 	
 	public void addItem(){
